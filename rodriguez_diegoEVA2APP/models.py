adfc8a7e76e3d5e1eb63f7756bb04bca4fefb420
Diego Rodriguez <165180817+KirinRyust@users.noreply.github.com>
2024-11-05 20:20:49 -0300

    Add files via upload

--- rodriguez_diegoEVA2APP/models.py
+++ rodriguez_diegoEVA2APP/models.py
@@ -8,4 +8,4 @@ class Django_Seminario(models.Model):
     empresaInstitucion = models.CharField(max_length=50)
     email = models.EmailField()
     profesion = models.CharField(max_length=50)
-    observaciones = models.CharField(max_length=200)
+    observaciones = models.CharField(max_length=200, blank=True, null=True)
